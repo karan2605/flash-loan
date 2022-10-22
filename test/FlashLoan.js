4b6e06358ef1a2ff34ce47cb71e2d48b3c3be881
Karan Patel <karan2605@hotmail.co.uk>
2022-10-22 14:14:54 +0100

    deploy pool, approve deposit and then deposit

--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -10,10 +10,28 @@ const ether = tokens
 describe('FlashLoan', () => {
     
     beforeEach(async () => {
+        // Setup accounts
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+         
         // Load accounts
         const FlashLoan = await ethers.getContractFactory('FlashLoan')
         const FlashLoanReceiver = await ethers.getContractFactory('FlashLoanReceiver')
         const Token = await ethers.getContractFactory('Token')
+
+        // Deploy Token
+        token = await Token.deploy('Karans Token', 'KPTOK', '1000000')
+
+        // Deploy Flash Loan Pool
+        flashLoan = await FlashLoan.deploy(token.address)
+
+        // Approve tokens before depositing
+        let transaction = await token.connect(deployer).approve(flashLoan.address, tokens(1000000))
+        await transaction.wait()
+
+        // Deposit tokens into the pool 
+        transaction = await flashLoan.connect(deployer).despositTokens(tokens(1000000))
+        await transaction.wait()
 
     })
 
