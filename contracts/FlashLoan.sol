af2835e76159684006302a33b40f8f933a3f8cba
Karan Patel <karan2605@hotmail.co.uk>
2022-10-22 14:27:24 +0100

    check that at least 1 token is being deposited and add to the pool balance

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -3,15 +3,25 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "./Token.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract FlashLoan {
-    Token public token;
+    using SafeMath for uint256;
 
-    constructor(address _tokenAddress) public {
+    Token public token;
+    uint256 public poolBalance;
+
+    constructor(address _tokenAddress) {
         token = Token(_tokenAddress);
     }
 
     function despositTokens(uint256 _amount) external {
+        require(_amount > 0, "Must deposit at least one token");
         token.transferFrom(msg.sender, address(this), _amount);
+        poolBalance = poolBalance.add(_amount);
+    }
+
+    function flashLoan(uint256 _borrowAmount) external {
+        
     }
 }
