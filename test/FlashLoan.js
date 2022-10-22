56e04dbc7789849443585f01d87ed83ecf45e0f2
Karan Patel <karan2605@hotmail.co.uk>
2022-10-22 13:12:33 +0100

    Test file for flash loan, imported contracts

---
+++ test/FlashLoan.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('FlashLoan', () => {
+    
+    beforeEach(async () => {
+        // Load accounts
+        const FlashLoan = await ethers.getContractFactory('FlashLoan')
+        const FlashLoanReceiver = await ethers.getContractFactory('FlashLoanReceiver')
+        const Token = await ethers.getContractFactory('Token')
+
+    })
+
+    describe('Deployment', () => {
+
+    })
+})
